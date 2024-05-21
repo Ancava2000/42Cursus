1e2417b31f980abc8466b9f1f97f13cbd81fb251
Ana Carro <anacarrovaz@gmail.com>
2024-05-21 17:18:52 +0200

    Update ft_lstlast_bonus.c

--- libft/ft_lstlast_bonus.c
+++ libft/ft_lstlast_bonus.c
@@ -22,3 +22,13 @@ t_list	*ft_lstlast(t_list *lst)
 	}
 	return (lst);
 }
+
+/*
+int main ()
+{
+	t_list *lst = ft_lstnew("Hello");   Create a new node (lst) with the content "Hello".
+	ft_lstadd_back(&lst, ft_lstnew("World"));  It adds the content "World" at the end of the existing list (lst).
+	t_list *last_elem = ft_lstlast(lst);    Return: The node *lst_elem points to the content "World".
+ 	return (0);
+}
+*/
