e903790c473a90988942453c0586a3bfbeaa5feb
Ana Carro <anacarrovaz@gmail.com>
2024-10-08 12:53:29 +0200

    Update cases_cost_ab.c

--- Push_swap/cases_cost_ab.c
+++ Push_swap/cases_cost_ab.c
@@ -12,6 +12,10 @@
 
 #include "push_swap.h"
 
+/* This function calculates the number of moves needed to put the nodes in first position. 
+First, the i variable stores the index the node "a" will get in the stack "b", once it's pushed.
+Second, if that value is less than the actual index of node "a" in the stack "a", the variable would
+store that new value. Basically, this function returns the biggest number depending of both indexes */
 int	ft_case_rarb_b(t_stack *a, t_stack *b, int nbr)
 {
 	int	i;
@@ -22,6 +26,10 @@ int	ft_case_rarb_b(t_stack *a, t_stack *b, int nbr)
 	return (i);
 }
 
+/* This function calculates the number of moves needed to put the nodes in first position. 
+First, the i variable stores the reverse index the node "a" will get in the stack "b", once it's pushed.
+Second, if that value is less than the actual reverse index of node "a" in the stack "a", the variable would
+store that new value. Basically, this function returns the biggest number depending of both indexes */
 int	ft_case_rrarrb_b(t_stack *a, t_stack *b, int nbr)
 {
 	int	i;
@@ -34,6 +42,11 @@ int	ft_case_rrarrb_b(t_stack *a, t_stack *b, int nbr)
 	return (i);
 }
 
+/* This function calculates the number of moves needed to put the nodes in first position. 
+First, the i variable stores the actual reverse index of the node "a".
+Second, the i variable stores the sum of the index the node "a" will get in the stack "b" plus the reverse index. 
+Basically, this function returns the biggest number depending of both indexes.
+In this case, we do the sum because this case can't make both moves at the same time */
 int	ft_case_rrarb_b(t_stack *a, t_stack *b, int nbr)
 {
 	int	i;
@@ -45,6 +58,11 @@ int	ft_case_rrarb_b(t_stack *a, t_stack *b, int nbr)
 	return (i);
 }
 
+/* This function calculates the number of moves needed to put the nodes in first position. 
+First, the i variable stores the reverse index the node "a" will get in the stack "b"
+Second, the i variable stores the sum of the actual index of the node "a" plus the reverse index. 
+Basically, this function returns the biggest number depending of both indexes.
+In this case, we do the sum because this case can't make both moves at the same time */
 int	ft_case_rarrb_b(t_stack *a, t_stack *b, int nbr)
 {
 	int	i;
