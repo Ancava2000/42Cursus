d3901493277ffec6d4657f1566a239b0eec3bc81
Ana Carro <anacarrovaz@gmail.com>
2024-10-18 11:19:38 +0200

    Update get_next_line.c

--- push_swap/libft/get_next_line.c
+++ push_swap/libft/get_next_line.c
@@ -156,26 +156,4 @@ int	main(void)
 	close(fd);
 	return (0);
 }
-
-stdin?
-void s_gets(char* str, int n)
-{
-    char* str_read = fgets(str, n, stdin);
-    if (!str_read)
-        return;
-
-    int i = 0;
-    while (str[i] != '\n' && str[i] != '\0')
-        i++;
-
-    if (str[i] == '\n')
-        str[i] = '\0';
-}
-
-int main()
-{
-    char my_string[11];
-    s_gets(my_string, 11);
-    printf("my_string = %s\n", my_string);
-}
 */
