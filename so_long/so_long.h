5850dad23345835634cde6725474f9e7c68d5904
Ana Carro <anacarrovaz@gmail.com>
2024-11-19 13:33:13 +0100

    Update so_long.h

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -13,6 +13,7 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+// COLORS
 # define RED "\033[1;91m" 
 # define CYAN "\033[1;96m"
 # define BLUE "\033[0;34m" 
@@ -24,14 +25,16 @@
 # define MOVE	64
 # define BUFF_SIZE 10000
 
-# define ERROR_PARAM RED "Error\nIncorrect number of parameters\n" RESET
-# define ERROR_BER RED "Error\nFile has invalid file extension\n" RESET
-# define ERROR_EMPTY RED "Error\nMap is empty\n" RESET
-# define ERROR_RECT RED "Error\nMap is not rectangle.\n" RESET
-# define ERROR_WRONG_COLLECT RED "Error\nMap have the wrongs components\n" RESET
-# define ERROR_WALL RED "Error\nMap don't have close walls\n" RESET
-# define ERROR_NB_COLLECT RED "Error\nYou don't have the correct components\n" RESET
-# define ERROR_PATH RED RED "Error\nThere is not a valid path\n" RESET
+// ERROR MESSAGES
+# define ERROR_PARAM "\033[1;91mError\nIncorrect number of parameters\n\033[0m"
+# define ERROR_BER  "\033[1;91mError\nFile has invalid file extension\n\033[0m"
+# define ERROR_NO_LINE  "\033[1;91mError\nMap has an empty line\n\033[0m" 
+// # define ERROR_EMPTY  "\033[1;91mError\nMap is empty\n\033[0m" 
+# define ERROR_RECT "\033[1;91mError\nMap is not a rectangle.\n\033[0m" 
+# define ERROR_WRONG_COL "\033[1;91mError\nWrongs components\n\033[0m"
+# define ERROR_WALL  "\033[1;91mError\nMap doesn't have close walls\n\033[0m" 
+# define ERROR_NB_COL "\033[1;91mError\nIncorrect number of components\n\033[0m"
+# define ERROR_PATH "\033[1;91mError\nThere is not a valid path\n\033[0m"
 
 # include <stdlib.h>
 # include <stdio.h>
@@ -40,7 +43,7 @@
 # include "MLX42/include/MLX42/MLX42.h"
 # include "Libft/inc/libft/libft.h"
 
-//estructura texturas
+//textures
 typedef struct s_texture
 {
 	mlx_texture_t	*wall;
@@ -49,9 +52,10 @@ typedef struct s_texture
 	mlx_texture_t	*open_exit;
 	mlx_texture_t	*close_exit;
 	mlx_texture_t	*player;
+	mlx_texture_t	*alien;
 }	t_texture;
 
-//estructura imágenes
+//images
 typedef struct s_image
 {
 	mlx_image_t		*wall;
@@ -60,10 +64,11 @@ typedef struct s_image
 	mlx_image_t		*player;
 	mlx_image_t		*open_exit;
 	mlx_image_t		*close_exit;
+	mlx_image_t		*alien;
 }	t_image;
 
-//estructura datos del mapa
-//Widht = y 
+//map data
+//Width = y 
 //Height = x
 typedef struct s_map
 {
@@ -77,28 +82,37 @@ typedef struct s_map
 	int					width;
 	int					height;
 	int					c_moves;
-	int					c_collectible;
-	int					t_collectible;
+	int					c_collectible; //what the player has
+	int					t_collectible; //the total
 }	t_map;
 
+// Main an initialization
 int		main(int argc, char **argv);
-void	ft_size_map(t_map *map, char **array);
+// Read map and checkers map
+int		ft_check_ber(char *fn);
 char	**ft_read_map(char *fn);
 int		ft_map_check(char **array);
-int		ft_check_ber(char *fn);
+void	ft_size_map(t_map *map, char **array);
+int		ft_check_path(t_map *map, char *fn);
 int		ft_min_elements(char **array);
+// Textures and images
 void	ft_get_textures(t_map *map);
 void	ft_get_images(t_map *map);
 void	ft_delete_textu(t_map *map);
+// Render images
 void	ft_print_elements(t_map *map, t_image *image);
 void	ft_print_floor(t_map *map, t_image *image);
-//void	ft_render_exit(int y, int x, t_map *map);
-//void	ft_close_exits(t_map *map);
+// Movements
 void	ft_key_hook(mlx_key_data_t keydata, void *param);
+void	ft_moves(t_map *map);
+//Enemies
+void	ft_alien(t_map *map);
+// Collectibles check
 void	ft_count_collectibles(t_map *map);
 int		ft_total_collectibles(t_map *map);
-void	ft_check_win(t_map *map);
-void	ft_moves(t_map *map);
+// Endscreen
+void	ft_check_ending(t_map *map);
+// Free
 void	ft_free_array(char **array);
 
 #endif
