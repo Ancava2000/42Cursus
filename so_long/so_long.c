c8acb7c0af376f0a3b64763c9c3afbe4a605e329
Ana Carro <anacarrovaz@gmail.com>
2024-11-19 13:32:33 +0100

    Update so_long.c

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -16,7 +16,6 @@ int	ft_initialize(t_map *map)
 {
 	map -> c_moves = 0;
 	map -> c_collectible = 0;
-	map -> exit_image = NULL;
 	map -> mlx = mlx_init(map -> width, map -> height, "so_long", false);
 	if (!(map -> mlx))
 		return (FALSE);
@@ -46,8 +45,8 @@ int	main(int argc, char **argv)
 	if (ft_map_check(map.array) == FALSE)
 		return (ft_free_array(map.array), 1);
 	ft_size_map(&map, map.array);
-	//if (ft_check_valid_exit(&map, argv[1]) == FALSE)
-		//return (ft_free_array(map.array), 1);
+	if (ft_check_valid_exit(&map, argv[1]) == FALSE)
+		return (ft_free_array(map.array), 1);
 	if (ft_initialize(&map) == FALSE)
 		return (1);
 	return (0);
