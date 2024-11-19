d24f46b4c5b779d76f34637259b2c920f28620ba
Ana Carro <anacarrovaz@gmail.com>
2024-11-19 13:31:37 +0100

    Update generate_map_images.c

--- so_long/generate_map_images.c
+++ so_long/generate_map_images.c
@@ -21,6 +21,7 @@ void	ft_get_textures(t_map *map)
 	map->textu->player = mlx_load_png("./resources/player.png");
 	map ->textu->close_exit = mlx_load_png("./resources/close_exit.png");
 	map ->textu->open_exit = mlx_load_png("./resources/open_exit.png");
+	map ->textu->alien = mlx_load_png("./resources/alien.png");
 }
 
 void	ft_get_images(t_map *map)
@@ -34,6 +35,7 @@ void	ft_get_images(t_map *map)
 	map->img->close_exit = mlx_texture_to_image(map->mlx,
 			map->textu->close_exit);
 	map->img->open_exit = mlx_texture_to_image(map->mlx, map->textu->open_exit);
+	map->img->alien = mlx_texture_to_image(map->mlx, map->textu->alien);
 	ft_delete_textu(map);
 }
 
@@ -45,4 +47,5 @@ void	ft_delete_textu(t_map *map)
 	mlx_delete_texture(map->textu->player);
 	mlx_delete_texture(map->textu->close_exit);
 	mlx_delete_texture(map->textu->open_exit);
+	mlx_delete_texture(map->textu->alien);
 }
