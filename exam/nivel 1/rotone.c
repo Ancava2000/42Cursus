2e0636c95ee90a4d5e6a93d529528a7f46df6141
Ana Carro <anacarrovaz@gmail.com>
2025-01-16 13:21:01 +0100

    Add files via upload

---
+++ exam/nivel 1/rotone.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotone.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acarro-v <acarro-v@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 12:38:51 by acarro-v          #+#    #+#             */
+/*   Updated: 2025/01/16 13:05:15 by acarro-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, <1);
+}
+
+void	rotone(char *s)
+{
+	while (*s)
+	{
+		if ((*s >= 'A' && *s <= 'Y') || (*s >= 'a' && *s <= 'y'))
+			ft_putchar(*s + 1);
+		else if (*s == 'Z' || *s == 'z')
+			ft_putchar(*s - 25);
+		else
+			ft_putchar (*s);
+		++s;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		rotone (argv[1]);
+	write (1, "\n", 1);
+	return (0);
+}
