f3b1b6acc7d4782830f1f3f1275cba9d35a5a916
Ana Carro <anacarrovaz@gmail.com>
2025-01-16 17:47:28 +0100

    Update rotone.c

--- exam/nivel 1/rotone.c
+++ exam/nivel 1/rotone.c
@@ -12,29 +12,25 @@
 
 #include <unistd.h>
 
-void ft_putchar(char c)
-{
-	write(1, &c, <1);
-}
-
-void	rotone(char *s)
-{
-	while (*s)
-	{
-		if ((*s >= 'A' && *s <= 'Y') || (*s >= 'a' && *s <= 'y'))
-			ft_putchar(*s + 1);
-		else if (*s == 'Z' || *s == 'z')
-			ft_putchar(*s - 25);
-		else
-			ft_putchar (*s);
-		++s;
-	}
-}
-
 int	main(int argc, char **argv)
 {
+	int	i;
+
+	i = 0;
 	if (argc == 2)
-		rotone (argv[1]);
+	{
+	    while (argv[1][i])
+	    {
+		    if (argv[1][i] >= 'A' && argv[1][i] <= 'Y')
+		        argv[1][i] += 1;
+		    else if (argv[1][i] >= 'a' && argv[1][i] <= 'y')
+			    argv[1][i] += 1;
+		    else if (argv[1][i] == 'Z' || argv[1][i] == 'z')
+			    argv[1][i] -= 25;
+		    write (1, &argv[1][i], 1);
+		    i++;
+	    }
+	}
 	write (1, "\n", 1);
 	return (0);
 }
