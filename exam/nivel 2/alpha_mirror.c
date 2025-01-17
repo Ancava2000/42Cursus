e6a983e0fb250bb1da3bc6a7a83b6b6fd750b9a3
Ana Carro <anacarrovaz@gmail.com>
2025-01-17 12:59:37 +0100

    Update alpha_mirror.c

--- exam/nivel 2/alpha_mirror.c
+++ exam/nivel 2/alpha_mirror.c
@@ -10,10 +10,10 @@ int	main(int argc, char **argv)
 	    while (argv[1][i])
 	    {
 		    if (argv[1][i] >= 'A' && argv[1][i] <= 'Y')
-          argv[1][i] = ('A' + 'Z') - argv[1][i];
+			    argv[1][i] = ('A' + 'Z') - argv[1][i];
 		    else if (argv[1][i] >= 'a' && argv[1][i] <= 'y')
 			    argv[1][i] = ('a' + 'z') - argv[1][i];
-			  write (1, &argv[1][i], 1);
+		    write (1, &argv[1][i], 1);
 		    i++;
 	    }
 	}
