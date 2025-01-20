3c4ea72c6af4795710e0e1e44e1d6e31ef46f491
Ana Carro <anacarrovaz@gmail.com>
2025-01-20 21:45:57 +0100

    Create expand_str.c

---
+++ exam/nivel 3/expand_str.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    int i;
+    int flag;
+    
+    if (argc == 2)
+    {
+        i = 0;
+        flag = 0;
+        while (argv[1][i] == 32 || argv[1][i] == 9)
+            i++;
+        while (argv[1][i])
+        {
+            if (argv[1][i] == 32 || argv[1][i] == 9)
+                flag = 1;
+            if (!(argv[1][i] == 32 || argv[1][i] == 9))
+            {
+                if (flag)
+                    write (1, "   ", 3);
+                flag = 0;
+                write (1, &argv[1][i], 1);
+            }
+            i++;
+        }
+    }
+    write (1, "\n", 1);
+    return (0);
+}
