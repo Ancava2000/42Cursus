603e53bf0fd4306860e41894503ac72e6d5ad563
Ana Carro <anacarrovaz@gmail.com>
2025-01-20 21:56:50 +0100

    Create ft_atoi_base.c

---
+++ exam/nivel 3/ft_atoi_base.c
@@ -0,0 +1,31 @@
+int	ft_atoi_base(const char *str, int str_base)
+{
+    int i;
+    int sign;
+    int c;
+    int res;
+    
+    i = 0;
+    sign = 1;
+    c = 0;
+    res = 0;
+    if (str[i] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+    while(str[i])
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+            c = str[i] - '0';
+        else if (str[i] >= 'a' && str[i] <= 'f')
+            c = (str[i] - 'a') + 10;
+        else if (str[i] >= 'A' && str[i] <= 'F')
+            c = (str[i] - 'A') + 10;
+        else
+            break;
+        res = res * str_base + c;
+        i++;
+    }
+    return (sign * res);
+}
