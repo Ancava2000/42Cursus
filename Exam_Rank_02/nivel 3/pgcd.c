819c38df8583c4d300c3d00fed04ffeca43cf058
Ana Carro <anacarrovaz@gmail.com>
2025-01-27 21:57:16 +0100

    Update pgcd.c

--- Exam_Rank_02/nivel 3/pgcd.c
+++ Exam_Rank_02/nivel 3/pgcd.c
@@ -3,23 +3,22 @@
 
 int ft_pgcd(int s1, int s2)
 {
-    if (s1 > 0 && s2 > 0)
+    unsigned int i;
+
+    i = s1
+    while (s1 > 0)
     {
-        while (s1 != s2)
-        {
-            if (s1 > s2)
-                s1 = s1 - s2;
-            else
-                s2 = s2 - s1;
-        }
-        printf("%d", s1);
+        if (s1 % i == 0 && s2 % i == 0)
+            return (i);
+        i--;
     }
+    return (1);
 }
 
 int main(int argc, char **argv)
 {
     if (argc == 3)
-        ft_pgcd(atoi(argv[1]), atoi(argv[2]));
+        printf ("%d", ft_pgcd(atoi(argv[1]), atoi(argv[2]));
     printf ("\n");
     return (0);
 }
