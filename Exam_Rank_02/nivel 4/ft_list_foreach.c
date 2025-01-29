0da7a2e2571cae66d84e67b06d8c251e0d42a8ec
Ana Carro <anacarrovaz@gmail.com>
2025-01-29 11:59:50 +0100

    Create ft_list_foreach.c

---
+++ Exam_Rank_02/nivel 4/ft_list_foreach.c
@@ -0,0 +1,11 @@
+#include "ft_list.h"
+
+void    ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+    while(begin_list != NULL)
+    {
+        if (begin_list -> data)
+            (*f)(begin_list -> data);
+        begin_list = begin_list -> next;
+    }
+}
