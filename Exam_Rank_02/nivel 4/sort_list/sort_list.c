f379b5ef3b152beea9e56e2253d7e0819003d25c
Ana Carro <anacarrovaz@gmail.com>
2025-01-30 14:24:03 +0100

    Update sort_list.c

--- Exam_Rank_02/nivel 4/sort_list/sort_list.c
+++ Exam_Rank_02/nivel 4/sort_list/sort_list.c
@@ -3,28 +3,28 @@
 
 void ft_swap(int *a, int *b)
 {
-    int temp;
+    int temp;    // there is not *
     
-    temp = *b;
+    temp = *b;  // REMEMBER THE *
     *b = *a;
     *a = temp;
 }
 
 t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
 {
-    t_list *temp;
+    t_list *temp;   // assign a *temp variable to store the first node
     
-    temp = lst;
-    while(lst -> next != NULL)
+    temp = lst;   // stores the top of the list
+    while(lst -> next != NULL)  // REMEMBER NULL
     {
-        if (((*cmp)(lst -> data, lst -> next -> data)) == 0)
+        if (((*cmp)(lst -> data, lst -> next -> data)) == 0)   // to sort the list. if the function with the parameters returns 0, the swap is made
         {
-            ft_swap(&lst -> data, &lst -> next -> data);
-            lst = temp;
+            ft_swap(&lst -> data, &lst -> next -> data);   // REMEMBER THE &
+            lst = temp;  // we start at the top of the list again
         }
-        else
+        else  // if the function doesn't return 0, we continue with the next node
             lst = lst -> next;
     }
-    lst = temp;
-    return(lst);
+    lst = temp;  // finally, we return the pointer to the start of the list
+    return(lst);  // returns the list from the top
 }
