d3c40f0f53aa261a2197b7f1cd2db34ab20e718a
Ana Carro <anacarrovaz@gmail.com>
2025-01-30 16:38:54 +0100

    Update ft_list_remove_if.c

--- Exam_Rank_02/nivel 4/ft_list_remove_if/ft_list_remove_if.c
+++ Exam_Rank_02/nivel 4/ft_list_remove_if/ft_list_remove_if.c
@@ -17,3 +17,50 @@ void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
     else
         ft_list_remove_if(&((*begin_list) -> next), data_ref, cmp);   // IMPORTANT, if they aren't equal we have to continue with the next node (&)
 }
+
+/*
+int cmp_int(void *a, void *b) 
+{
+    return *((int*)a) - *((int*)b); 
+}
+
+t_list *create_node(void *data)
+{
+    t_list *new_node = (t_list *)malloc(sizeof(t_list));
+    if (!new_node)
+        return NULL;
+    new_node->data = data;
+    new_node->next = NULL;
+    return new_node;
+}
+
+void print_list(t_list *begin_list)
+{
+    while (begin_list != NULL)
+    {
+        printf("%d -> ", *((int*)begin_list->data));
+        begin_list = begin_list->next;
+    }
+    printf("NULL\n");
+}
+
+int main()
+{
+    int num1 = 1;
+    int num2 = 2;
+    int num3 = 3;
+    int num4 = 2;
+
+    t_list *head = create_node(&num1);
+    head->next = create_node(&num2);
+    head->next->next = create_node(&num3);
+    head->next->next->next = create_node(&num4);
+    printf("Original list: ");
+    print_list(head);
+    int data_ref = 2;
+    ft_list_remove_if(&head, &data_ref, cmp_int);
+    printf("Modified list after removing 2: ");
+    print_list(head);
+    return (0);
+}
+*/
