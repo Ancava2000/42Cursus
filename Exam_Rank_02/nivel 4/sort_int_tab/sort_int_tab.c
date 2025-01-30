09e6a38a2c8e980e52481bcde5436da360abd71e
Ana Carro <anacarrovaz@gmail.com>
2025-01-30 14:18:46 +0100

    Update sort_int_tab.c

--- Exam_Rank_02/nivel 4/sort_int_tab/sort_int_tab.c
+++ Exam_Rank_02/nivel 4/sort_int_tab/sort_int_tab.c
@@ -2,7 +2,7 @@ void ft_swap(int *a, int *b)
 {
     int temp;  // temp is not a pointer
     
-    temp = *b;
+    temp = *b;  // REMEMBER THE *
     *b = *a;
     *a = temp;
 }
@@ -11,13 +11,13 @@ void sort_int_tab(int *tab, unsigned int size)
 {
     unsigned int i;
 
-    while (size > 0)
+    while (size > 0)  // iterate all the length
     {
         i = 0;
-        while (i < (size - 1))
+        while (i < (size - 1))  // iterate all the numbers in each iteration of the length
         {
             if (tab[i] > tab[i + 1])
-                ft_swap(&tab[i], &tab[i + 1]);
+                ft_swap(&tab[i], &tab[i + 1]);    // REMEMBER THE &
             i++;
         }
         size--;
