a4a2c4b2a8f4353e2f1415626162a57f0713d5dc
Ana Carro <anacarrovaz@gmail.com>
2025-01-30 13:16:04 +0100

    Update fprime.c

--- Exam_Rank_02/nivel 4/fprime/fprime.c
+++ Exam_Rank_02/nivel 4/fprime/fprime.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>  // printf
+#include <stdlib.h>  // atoi
 
 void ft_prime(int nb)
 {
-    unsigned i = 2;
+    unsigned i = 2;    //start with 2!!
     
     if (nb == 1)
         printf("1");
@@ -28,3 +28,11 @@ int main(int argc, char **argv)
     printf("\n");
     return (0);
 }
+
+/*
+12
+12 % 2 == 0      2 *
+6 % 2 == 0       2 * 2
+3 % 2 != 0 
+3 % 3 == 0       2 * 2 * 3
+*/
