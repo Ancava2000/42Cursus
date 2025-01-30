270bf3c6b77b6ad4162cd94251b861975640b992
Ana Carro <anacarrovaz@gmail.com>
2025-01-30 14:44:26 +0100

    Update ft_list_foreach.c

--- Exam_Rank_02/nivel 4/ft_list_foreach/ft_list_foreach.c
+++ Exam_Rank_02/nivel 4/ft_list_foreach/ft_list_foreach.c
@@ -9,3 +9,35 @@ void    ft_list_foreach(t_list *begin_list, void (*f)(void *))
         begin_list = begin_list -> next;
     }
 }
+
+/*
+#include <unistd.h>
+
+void ft_putstr(char *str)
+{
+    while(*str)
+        write (1, str++, 1);
+}
+
+t_list *create_node(void *data) 
+{
+    t_list *new_node = (t_list *)malloc(sizeof(t_list));
+    if (!new_node)
+        return NULL;
+    new_node->data = data;
+    new_node->next = NULL;
+    return (new_node);
+}
+
+int main() 
+{
+    t_list *head = create_node("Hello ");
+    head->next = create_node("42 ");
+    head->next->next = create_node("students ");
+    head->next->next->next = create_node("and ");
+    head->next->next->next->next = create_node("world!");
+    ft_list_foreach(head, (void *)ft_putstr);
+    write (1, "\n", 1);
+    return (0);
+}
+*/
