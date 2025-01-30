68c24995271f937887add10ff6406082d1e565dc
Ana Carro <anacarrovaz@gmail.com>
2025-01-30 14:00:58 +0100

    Update itoa.c

--- Exam_Rank_02/nivel 4/itoa/itoa.c
+++ Exam_Rank_02/nivel 4/itoa/itoa.c
@@ -9,7 +9,7 @@ int ft_length(int n)
         length = 1;  // For negative numbers, add space for the '-'
     while (n != 0)
     {
-        n /= 10;
+        n /= 10;  // reduces number
         length++;
     }
     return (length);
@@ -19,21 +19,21 @@ void ft_convert(int n, char *string, int length)
 {
     while (length > 0)
     {
-        string[length - 1] = n % 10 + '0';  // Store digit as char
-        n /= 10;
+        string[length - 1] = n % 10 + '0';  // store digit as char. -1 for the NULL character
+        n /= 10;   // reduces number
         length--;
     }
 }
 
 char *ft_itoa(int nbr)
 {
-    int sign;
-    int length;
-    long n;
-    char *string;
+    int sign;   // for the '-' if it is negative
+    int length;  // length of the int
+    long n;  // to store all the int
+    char *string; // the int convert to a string
 
-    if (nbr == -2147483648)
-        return "-2147483648";
+    if (nbr == -2147483648)   // special case
+        return ("-2147483648");
     sign = 0;
     n = nbr;
     if (nbr < 0)
@@ -42,12 +42,12 @@ char *ft_itoa(int nbr)
         n = -nbr;  // Work with the positive equivalent
     }
     length = ft_length(n);
-    string = (char *)malloc(sizeof(char) * (length + 1));
+    string = (char *)malloc(sizeof(char) * (length + 1)); // malloc with the size of a char * the length of the int +1 fot the NULL
     if (!string)
         return NULL;
     string[length] = '\0';  // Null-terminate the string
     ft_convert(n, string, length);
     if (sign)
         string[0] = '-';  // Add negative sign if necessary
-    return string;
+    return (string);
 }
