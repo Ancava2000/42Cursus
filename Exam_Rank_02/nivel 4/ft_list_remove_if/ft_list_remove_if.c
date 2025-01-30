1feb276569577b6adab1e03696a8511ddfcd7158
Ana Carro <anacarrovaz@gmail.com>
2025-01-30 13:22:57 +0100

    Update ft_list_remove_if.c

--- Exam_Rank_02/nivel 4/ft_list_remove_if/ft_list_remove_if.c
+++ Exam_Rank_02/nivel 4/ft_list_remove_if/ft_list_remove_if.c
@@ -1,19 +1,19 @@
 #include "ft_list.h"
-#include <stdlib.h>
+#include <stdlib.h>  // free
 
 void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-    t_list *temp;
+    t_list *temp;   // just one *
     
     if ((*begin_list == NULL) || (begin_list == NULL))
         return ;
-    if(cmp((*begin_list) -> data, data_ref) == 0)
+    if(cmp((*begin_list) -> data, data_ref) == 0)  // it frees the node if the function returns 0 (when both parameters are equal, ==)
     {
-        temp = *begin_list;
-        *begin_list = (*begin_list) -> next;
-        free(temp);
-        ft_list_remove_if(begin_list, data_ref, cmp);
+        temp = *begin_list;                        // temp is the first node
+        *begin_list = (*begin_list) -> next;       // the new first node in the list is now the second
+        free(temp);                                // it frees the first node
+        ft_list_remove_if(begin_list, data_ref, cmp);  // it repeats the process with the second node, now the first. Until the list ends
     }
     else
-        ft_list_remove_if(&((*begin_list) -> next), data_ref, cmp);
+        ft_list_remove_if(&((*begin_list) -> next), data_ref, cmp);   // IMPORTANT, if there aren't equal we have to continue with the next node (&)
 }
