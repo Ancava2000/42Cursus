c5cc62073849a47fa3af5bf9ec4af286e41eeed2
Ana Carro <anacarrovaz@gmail.com>
2025-01-30 14:09:50 +0100

    Update rev_wstr.c

--- Exam_Rank_02/nivel 4/rev_wstr/rev_wstr.c
+++ Exam_Rank_02/nivel 4/rev_wstr/rev_wstr.c
@@ -14,22 +14,22 @@ void rev_wstr(char *str)
     int word;
     
     i = 0;
-    word = 1;
-    while(str[i] != '\0')
+    word = 1;  // IMPORTANT starts with 1
+    while(str[i] != '\0')   // count all the string
         i++;
     while(i >= 0)
     {
         j = 0;
-        while(i >= 0 && (str[i] == '\0' || is_space(str[i])))
-            i--;
+        while(i >= 0 && (str[i] == '\0' || is_space(str[i])))  // count until the last letter of the last word. IMPORTANT == '\0'
+            i--;   // last letter of the last word
         j = i;
         while(j >= 0 && !is_space(str[j]))
-            j--;
+            j--;   // first letter of the last word
         if (!word)
             write (1, " ", 1);
-        write (1, str + j + 1, i - j);
-        word = 0;
-        i = j;
+        write (1, str + j + 1, i - j);   // 1, start of the word, length of the word
+        word = 0; // once we go through the first word, the following words are separated by " "
+        i = j;   // IMPORTANT i = j to start from the start of the last word
     }
 }
 
