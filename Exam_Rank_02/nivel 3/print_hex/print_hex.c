49628dd3b31ff65f187f42a78a637ff99a621897
Ana Carro <anacarrovaz@gmail.com>
2025-02-01 20:47:21 +0100

    Update print_hex.c

--- Exam_Rank_02/nivel 3/print_hex/print_hex.c
+++ Exam_Rank_02/nivel 3/print_hex/print_hex.c
@@ -5,25 +5,25 @@ int		ft_atoi(char *s)
 	unsigned int    result;
 	int		sign;
 
-    result = 0;
-    sign = 1;
-	while (*s == 32 || (*s >= 9 && *s <= 13))
-		s++;
+    	result = 0;
+    	sign = 1;
 	if (*s == '-')
 	    sign *= -1;
+	while (*s == 32 || (*s >= 9 && *s <= 13))
+		s++;
 	if (*s == '+' || *s == '-')
 	    s++;
 	while (*s >= '0' && *s <= '9')
 	{
 		result = result * 10 + *s - '0';
 		s++;
-    }
+    	}
 	return (sign * result);
 }
 
 void ft_hex(int n)
 {
-    char *hex = "0123456789abcdef";
+    char *hex = "0123456789abcdef";   // remember to write the char *
     
     if (n >= 16)
         ft_hex(n / 16);
@@ -34,7 +34,7 @@ void ft_hex(int n)
 int main(int argc, char **argv)
 {
     if (argc == 2)
-        ft_hex(ft_atoi(argv[1]));
+        ft_hex(ft_atoi(argv[1]));    // first convert to int, then to hex
     write (1, "\n", 1);
     return (0);
 }
