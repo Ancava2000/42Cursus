9e8cdd49724f481f0c12480f230ee013aba8519e
Ana Carro <anacarrovaz@gmail.com>
2025-02-01 20:14:04 +0100

    Update ft_atoi_base.c

--- Exam_Rank_02/nivel 3/ft_atoi_base/ft_atoi_base.c
+++ Exam_Rank_02/nivel 3/ft_atoi_base/ft_atoi_base.c
@@ -7,7 +7,7 @@ int	ft_atoi_base(const char *str, int str_base)
     
     i = 0;
     sign = 1;
-    c = 0;
+    c = 0;              // remember the int variable to stored char by char
     res = 0;
     if (str[i] == '-')
     {
@@ -17,14 +17,14 @@ int	ft_atoi_base(const char *str, int str_base)
     while(str[i])
     {
         if (str[i] >= '0' && str[i] <= '9')
-            c = str[i] - '0';
+            c = str[i] - '0';                  // convert char to int
         else if (str[i] >= 'a' && str[i] <= 'f')
-            c = (str[i] - 'a') + 10;
+            c = (str[i] - 'a') + 10;           // remember the "- 'a'"
         else if (str[i] >= 'A' && str[i] <= 'F')
             c = (str[i] - 'A') + 10;
         else
             break;
-        res = res * str_base + c;
+        res = res * str_base + c;  // res * 10 or 16 + the character (the char converted to int)
         i++;
     }
     return (sign * res);
