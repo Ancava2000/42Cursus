28268d278b536a9e482f0b19a6fd619613293f00
Ana Carro <anacarrovaz@gmail.com>
2025-02-06 07:45:49 -0500

    Add files via upload

---
+++ pipex/libft/ft_strncmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acarro-v <acarro-v@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/11 10:07:46 by acarro-v          #+#    #+#             */
+/*   Updated: 2024/04/23 10:53:28 by acarro-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+	{
+		if (s1[i] != s2[i])
+		{
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		}
+		i++;
+	}
+	if (i != n)
+	{
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
+}
---
+++ pipex/srcs/pipex.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acarro-v <acarro-v@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 11:58:51 by acarro-v          #+#    #+#             */
+/*   Updated: 2025/02/06 13:16:38 by acarro-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex.h"
+
+void	child(char **argv, int *fd, char **envp)
+{
+	int	filein;
+
+	filein = open(argv[1], O_RDONLY);
+	if (filein == -1)
+		error();
+	dup2(filein, 0);
+	dup2(fd[1], 1);
+	close(fd[0]);
+	close(filein);
+	execute(argv[2], envp);
+}
+
+void	parent(char **argv, int *fd, char **envp)
+{
+	int	fileout;
+
+	fileout = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0777);
+	if (fileout == -1)
+		error();
+	dup2(fileout, 1);
+	dup2(fd[0], 0);
+	close(fd[1]);
+	close(fileout);
+	execute(argv[3], envp);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int		fd[2];
+	pid_t	pid;
+
+	if (argc == 5)
+	{
+		if (pipe(fd) == -1)
+			error();
+		pid = fork();
+		if (pid == -1)
+			error();
+		if (pid == 0)
+			child(argv, fd, envp);
+		waitpid(pid, NULL, 0);
+		parent(argv, fd, envp);
+	}
+	else
+	{
+		ft_putstr_fd("\033[31mError: Bad arguments\n\e[0m", 2);
+		ft_putstr_fd("Ex: ./pipex <file1> <cmd1> <cmd2> <file2>\n", 1);
+	}
+	return (0);
+}
