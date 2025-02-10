0cd37ff295a8567e486f293b135f026bee8e05ad
Ana Carro <anacarrovaz@gmail.com>
2025-02-10 13:19:05 +0100

    Create pipex_bonus.h

---
+++ pipex/includes/pipex_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acarro-v <acarro-v@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 13:02:23 by acarro-v          #+#    #+#             */
+/*   Updated: 2025/02/03 13:36:10 by acarro-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include "../libft/libft.h"
+
+/* Bonus functions */
+void	error(void);
+char	*find_path(char *cmd, char **envp);
+void	execute(char *argv, char **envp);
+int  get_next_line(char *argv);
+
+#endif
