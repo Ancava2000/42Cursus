7bb1ac5b95a5fee7edfd2abd4d4457258ecac3f6
Ana Carro <anacarrovaz@gmail.com>
2025-02-20 13:40:52 -0500

    Update ft_itoa.c

--- Exam_Rank_02/nivel 4/ft_itoa/ft_itoa.c
+++ Exam_Rank_02/nivel 4/ft_itoa/ft_itoa.c
@@ -2,44 +2,44 @@
 
 char	*ft_itoa(int nbr)
 {
-    int len;
-    int temp;
-    int is_negative = 0;
+	int len;
+	int temp;
+	int is_negative = 0;
 
-    if (nbr == -2147483648)
-        return ("-2147483648");
-    if (nbr < 0)
-    {
-        is_negative = 1;
-        nbr = -nbr;
-    }
-    temp = nbr;
+	if (nbr == -2147483648)
+		return ("-2147483648");
+    	if (nbr < 0)
+    	{
+        	is_negative = 1;
+        	nbr = -nbr;
+    	}
+    	temp = nbr;
 	len = 0;
-    while (temp != 0)
-    {
-        temp /= 10;
-        len++;
-    }
-    len += is_negative;
-    char *str = (char *)malloc(len + 1);
-    if (!str)
-        return (NULL);
-    str[len] = '\0';
-    if (nbr == 0)
-    {
-        str[0] = '0';
-        return (str);
-    }
+    	while (temp != 0)
+    	{
+        	temp /= 10;
+        	len++;
+    	}
+    	len += is_negative;
+    	char *str = (char *)malloc(len + 1);
+    	if (!str)
+        	return (NULL);
+    	str[len] = '\0';
+    	if (nbr == 0)
+    	{
+        	str[0] = '0';
+        	return (str);
+    	}
 	len -= 1;
-    while (nbr > 0)
-    {
-        str[len] = (nbr % 10) + '0';
-        nbr /= 10;
+    	while (nbr > 0)
+	{
+        	str[len] = (nbr % 10) + '0';
+        	nbr /= 10;
 		len--;
-    }
-    if (is_negative)
-        str[0] = '-';
-    return (str);
+    	}
+    	if (is_negative)
+        	str[0] = '-';
+    	return (str);
 }
 
 
