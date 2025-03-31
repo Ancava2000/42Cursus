de4a2ea292fe9a3c7b7e1ddc54e34d48ee78fef0
Ana Carro <anacarrovaz@gmail.com>
2025-03-31 22:05:48 +0200

    Update simulation.c

--- philosophers/simulation.c
+++ philosophers/simulation.c
@@ -1,20 +1,4 @@
 void  launcher(t_table *table)
 {
-  int i;
 
-  i = -1;
-  if (table -> nb_philos == 1)
-    ;
-  else
-    while (++i < table -> nb_philos)
-      {
-        if (phtread_create(&table -> philos[i].thread_id, NULL, &philo_routine,  &table -> philos[i]) != 0)
-            ft_error("Error creating thread");
-      }
-    i = -1
-    while (++i < table -> nb_philos)
-    {
-      if (phtread_join(table -> philos[i].thread_id, NULL) != 0)
-          ft_error("Error joining threads");
-    }
 }
