503d8a46a6425378694c377e7bbed76b0163b267
Ana Carro <anacarrovaz@gmail.com>
2025-04-01 02:38:35 +0200

    Update locks.c

--- philosophers/locks.c
+++ philosophers/locks.c
@@ -1,9 +1,9 @@
 #include "philo.h"
 
-void set_value_bool(t_mtx *mutex, int *dest, int nb)
+void set_value_bool(t_mtx *mutex, long *dest, long value)
 {
     pthread_mutex_lock(mutex, NULL);
-    *dest = nb;
+    *dest = value;
     pthread_mutex_unlock(mutex, NULL);
 }
 
