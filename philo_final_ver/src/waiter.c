fef0e9ddc4cfb95ec9c4fdbcce1eb812713c2cfa
Ana Carro <anacarrovaz@gmail.com>
2025-04-24 14:55:31 -0400

    Update waiter.c

--- philo_final_ver/src/waiter.c
+++ philo_final_ver/src/waiter.c
@@ -38,12 +38,10 @@ int	check_dead(t_table *table, unsigned int i)
 {
 	unsigned long long dead_time;
 
-	pthread_mutex_lock(&table->lock_meal);
-	dead_time = table->philos[i].last_eaten - get_now_time() + table->time_to_die;
-	pthread_mutex_unlock(&table->lock_meal);
 	if (table->philos[i].dead_status)
 		return (1);
 	pthread_mutex_lock(&table->lock_meal);
+	dead_time = table->philos[i].last_eaten - get_now_time() + table->time_to_die;
 	if (dead_time <= 0 && !table->philos[i].full)
 	{
 		messages(6, i + 1, get_now_time() - table->dinner_start, table);
@@ -92,14 +90,13 @@ void	check_all_full(t_table *table)
 void	check_full(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->table->lock_dead);
-	pthread_mutex_lock(&philo->table->lock_meal);
 	if (philo->dead_status || philo->full)
 	{
 		pthread_mutex_unlock(&philo->table->lock_dead);
-		pthread_mutex_unlock(&philo->table->lock_meal);
 		return ;
 	}
 	pthread_mutex_unlock(&philo->table->lock_dead);
+	pthread_mutex_lock(&philo->table->lock_meal);
 	if (philo->meals_eaten == philo->table->nb_meals)
 	{
 		messages(5, philo->philo_id, get_now_time() - philo->table->dinner_start, philo->table);
